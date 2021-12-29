f8dcfe79079befd550038bd3b104e92994d6d474
Mayar Abu Saleh <mayarabusalh@gmail.com>
2021-12-29 13:29:36 +0200

    not index

--- js/pages/Nots-App/app-nots.jsx
+++
@@ -1,7 +0,0 @@
-import {Details} from  '../apps/keep/pages/note-details'
-import {Index} from  '../apps/keep/pages/note-index'
-
-
-export class Nots extends React.Component {
-    
-}
---
+++ js/pages/app-nots.jsx
@@ -0,0 +1,20 @@
+import { Details } from '../apps/keep/pages/note-details';
+import { Index } from '../apps/keep/pages/note-index';
+import {Notes} from '../apps/keep/services/note.service'
+
+
+state = {
+    notes : Notes
+}
+
+// componentDidMount() {
+//     this.loadNots();
+// }
+
+// export class Nots extends React.Component {
+//   render() {
+//     return (
+
+//     )
+//   }
+// }
