99782f92a3591715fd1fbb8b01ede9becbeec45a
Mayar Abu Saleh <mayarabusalh@gmail.com>
2021-12-29 10:57:11 +0200

    home work

---
+++ js/pages/app-home.jsx
@@ -0,0 +1,15 @@
+export class Home extends React.Component{
+    state = {
+
+    }
+    render(){
+        return(
+            <section>
+                <header>
+                    <h1></h1>
+                    <nav></nav>
+                </header>
+            </section>
+        )
+    }
+}
