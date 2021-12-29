e008c919b87931b803086e3eaf42765d61c39dbc
itshak <itshak.012@gmail.com>
2021-12-29 16:50:20 +0200

    add about

--- js/apps/email/pages/email-index.jsx
+++ js/apps/email/pages/email-index.jsx
@@ -2,20 +2,20 @@
 // import {Index} from  '../apps/keep/pages/note-index'
 
 
-// export class Mail extends React.Component {
-//     render() {
+export class Mail extends React.Component {
+    render() {
        
-//         return (
-//             <section className="">
+        return (
+            <section className="mails">
                 
-//                 <form onSubmit={this.onSaveCar} >
-//                     {/* <label htmlFor="by-vendor">Vendor:</label>
-//                     <input ref={this.inputRef} placeholder="Enter vendor" name="vendor" type="text" id="by-vendor" value={vendor} onChange={this.handleChange} />
-//                     <label htmlFor="by-speed">Speed:</label>
-//                     <input placeholder="Enter speed" name="speed" type="number" id="by-speed" value={speed} onChange={this.handleChange} />
-//                     <button className="primary-btn ">Save car</button> */}
-//                 </form>
-//             </section>
-//         )
-//     }
-// }
+                <form onSubmit={this.onSaveCar} >
+                    {/* <label htmlFor="by-vendor">Vendor:</label>
+                    <input ref={this.inputRef} placeholder="Enter vendor" name="vendor" type="text" id="by-vendor" value={vendor} onChange={this.handleChange} />
+                    <label htmlFor="by-speed">Speed:</label>
+                    <input placeholder="Enter speed" name="speed" type="number" id="by-speed" value={speed} onChange={this.handleChange} />
+                    <button className="primary-btn ">Save car</button> */}
+                </form>
+            </section>
+        )
+    }
+}
---
+++ js/pages/app-about.jsx
@@ -0,0 +1,14 @@
+
+
+export function About() {
+    return (
+        <section className="about">
+            <h1>We're all about cars...</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit earum tenetur, accusantium adipisci, provident reprehenderit eaque similique quia nemo commodi et laboriosam ab est sunt dolor velit repellat illum placeat.</p>
+            {/* <NavLink activeClassName="my-active" to="/about/team">My Team</NavLink>
+            <NavLink activeClassName="my-active" to="/about/vision">Our vision</NavLink>
+            <Route component={Team} path="/about/team" />
+            <Route component={Vision} path="/about/vision" /> */}
+        </section>
+    )
+}
