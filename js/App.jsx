c250fc20fa1ad96433b142160861c2d84b20625a
itshak <itshak.012@gmail.com>
2021-12-29 14:04:17 +0200

    test3

--- js/App.jsx
+++ js/App.jsx
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import { Home } from './pages/app-home.jsx'
 import { About } from './pages/app-about.jsx'
 import { Nots } from './pages/app-nots.jsx'
 import { Mail } from './pages/app-mail.jsx'
-=======
-import {Home} from './pages/app-home.jsx'
-import {About} from './pages/app-about.jsx'
-import {Nots} from './pages/Nots-App/app-nots.jsx'
-import {Eamil} from './pages/Email-App/app-email.jsx'
->>>>>>> f8dcfe79079befd550038bd3b104e92994d6d474
+
+import { AppHeader } from './cmps/AppHeader.jsx'
+
 
 const Router = ReactRouterDOM.HashRouter;
 const { Route, Switch } = ReactRouterDOM;
@@ -17,13 +13,9 @@ export function App() {
   return (
     <Router>
       <section className='app'>
-        {/* <AppHeader /> */}
+        <AppHeader />
         <main>
           <Switch>
-<<<<<<< HEAD
-=======
-            <Route component={Eamil} path='/Email' />
->>>>>>> f8dcfe79079befd550038bd3b104e92994d6d474
             <Route component={Nots} path='/Nots' />
             <Route component={Mail} path='/Mail' />
             <Route component={About} path='/About' />
