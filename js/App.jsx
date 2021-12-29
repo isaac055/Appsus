8448ebfbe06a8eef3ac1c99b9c8475064d6939e6
Mayar Abu Saleh <mayarabusalh@gmail.com>
2021-12-29 11:28:51 +0200

    home css edt

--- js/App.jsx
+++ js/App.jsx
@@ -2,16 +2,17 @@ import {Home} from './pages/app-home.jsx'
 import {About} from './pages/app-about.jsx'
 import {Nots} from './pages/app-nots.jsx'
 
-
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
 
 export function App() {
   return (
     <Router>
       <section className='app'>
-        <AppHeader />
+        {/* <AppHeader /> */}
         <main>
           <Switch>
-            <Route component={} path='' />
+            {/* <Route component={} path='' /> */}
             <Route component={Nots} path='/Nots' />
             <Route component={About} path='/About' />
             <Route component={Home} path='/' />
--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -1,15 +1,23 @@
-export class Home extends React.Component{
-    state = {
-
-    }
-    render(){
-        return(
-            <section>
-                <header>
-                    <h1></h1>
-                    <nav><i class="fab fa-buromobelexperte"></i></nav>
-                </header>
-            </section>
-        )
-    }
+export class Home extends React.Component {
+  state = {};
+  render() {
+    return (
+      <section className = 'homePage flex align justify'>
+        <header>
+          <h1>Appsus</h1>
+          <nav>
+            <i className ='fab fa-buromobelexperte'></i>
+          </nav>
+        </header>
+        <main>
+          <h1>Welcome</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde
+            recusandae eligendi ab illum possimus similique quaerat nobis fugiat
+            saepe eius.
+          </p>
+        </main>
+      </section>
+    );
+  }
 }
