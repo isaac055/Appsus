9571718585598bcd83ddf2fe2a9380406c22f261
itshak <itshak.012@gmail.com>
2021-12-30 14:37:41 +0200

    2

--- js/apps/email/cmps/email-preview.jsx
+++ js/apps/email/cmps/email-preview.jsx
@@ -3,12 +3,14 @@ const { Link } = ReactRouterDOM
 export function EmailPreview({ email }) {
     console.log(email);
     return (
-        
+
         <Link className="clean-link" to={`/email/${email.id}`}>
             <div className="email-preview">
                 <img src={`https://robohash.org/${email.id}`} alt="" />
-                <h4>{email.subject}</h4>
-                <h5>{email.body}</h5>
+                <div className="email-content">
+                    <h4>{email.subject}</h4>
+                    <h5>{email.body}</h5>
+                </div>
             </div>
         // </Link>
     )
